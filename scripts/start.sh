c30a3706f89c823c90a4318f03f22a34d4f25e33
althaf-dev <althafbandiode@gmail.com>
2025-12-19 22:44:30 +0530

    Refactor start.sh to set error handling, define APP_DIR, and ensure correct script execution

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-pm2 stop all || true
-pm2 start src/app.js
-pm2 save
+set -e
+
+APP_DIR=/home/ubuntu/app
+cd $APP_DIR
+
+/usr/bin/pm2 stop all || true
+/usr/bin/pm2 start src/app.js --name app
+/usr/bin/pm2 save
